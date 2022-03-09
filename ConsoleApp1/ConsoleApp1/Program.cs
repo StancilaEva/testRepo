0f10ec8cadee74ff518c423eb36d031fd745bb9a
Eva Stancila <eva.stancila@amdaris.com>
2022-03-09 16:17:51 +0200

    deleted AnotherClass

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("user 1 added this");
+﻿Console.WriteLine("user 1 added this");
 int x = 2;
